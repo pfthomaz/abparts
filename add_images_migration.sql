bb3a2fce65c1355495dbf47f289568bac5759290
Diogo <diogo.thomaz@gmail.com>
2025-11-21 20:35:34 +0200

    Remove large files from git and update .gitignore

--- add_images_migration.sql
+++
@@ -1,5 +0,0 @@
--- Add logo_url to organizations table
-ALTER TABLE organizations ADD COLUMN IF NOT EXISTS logo_url VARCHAR(500);
-
--- Add profile_photo_url to users table
-ALTER TABLE users ADD COLUMN IF NOT EXISTS profile_photo_url VARCHAR(500);
--- fix_organizations_table.sql
+++
@@ -1,2 +0,0 @@
--- Add missing country column to organizations table
-ALTER TABLE organizations ADD COLUMN IF NOT EXISTS country VARCHAR(3);
