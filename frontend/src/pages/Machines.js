d309fcd1b3e5c1616a4b48388b842ab0ee00d6fc
Diogo <diogo.thomaz@gmail.com>
2025-11-22 18:23:22 +0200

    Update Machines.js

--- frontend/src/pages/Machines.js
+++ frontend/src/pages/Machines.js
@@ -37,7 +37,7 @@ const Machines = () => {
     try {
       const [machinesData, orgsData] = await Promise.all([
         machinesService.getMachines(),
-        api.get('/organizations'),
+        api.get('/organizations/'),
       ]);
       setMachines(machinesData);
       setOrganizations(orgsData);
