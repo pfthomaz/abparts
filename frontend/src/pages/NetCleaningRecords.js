e23da10b2ae3316cae38bcd63006e23e2dd63750
Diogo <diogo.thomaz@gmail.com>
2026-04-29 09:39:28 +0300

    Update NetCleaningRecords.js

--- frontend/src/pages/NetCleaningRecords.js
+++ frontend/src/pages/NetCleaningRecords.js
@@ -240,7 +240,7 @@ const NetCleaningRecords = () => {
         </select>
       </div>
 
-      <div className="bg-white rounded-lg shadow overflow-hidden">
+      <div className="bg-white rounded-lg shadow overflow-x-auto">
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
